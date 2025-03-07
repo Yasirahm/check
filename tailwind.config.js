abf6f5b05fe586f479d1c5f742d89e80175b6d31
Yasir Hamid <ratherseenu16@gmail.com>
2025-03-07 08:40:47 +0530

    Updated files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+// tailwind.config.js
 module.exports = {
-  content: ["./src//*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // ✅ Correct (Include all nested files)
+    "./public/index.html",
+  ]
+  ,
   theme: {
     extend: {},
   },
